968d52f281cea8c3285742583eed8416d95d0126
kalbert83 <mat17031@byui.edu>
2019-09-24 14:34:40 -0600

    testing

--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
       <link rel="stylesheet" href="index.css">
       <title>cs313 Kalbert Mata</title>
   </head>
@@ -10,7 +11,8 @@
     <header> 
     <h3>CS 313 BYUI</h3>
       <nav>
-          <ul class = "mainmenu">
+          <ul class="mainmenu">
+          <li class="menubutton"><a href="#" title="Menu" onclick="toggleMenu()">&#9776; Menu</a><li>
               <li><a href="index.php">Home</a></li>
               <li><a href="">About</a></li>
               <li><a href="assingments.php">Assingments</a></li>
@@ -18,11 +20,14 @@
       </nav>
     </header>
     <main>
-      <h1>Kablbet Mata</h1>
+      <h1>Kalbert Mata</h1>
       <p>On this space i will talk about myself</p>
       <img src="images/image1.jpg" alt="profile">
     </main>
-    <script src="hamburger.js"></script>
+    
   
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
   </body>
   </html>
