4681b61f8e29739812062115ead12324a5cd07fe
kalbert83 <mat17031@byui.edu>
2019-09-24 14:10:32 -0600

    testing

--- web/index.php
+++ web/index.php
@@ -1,5 +1,28 @@
-<?php
-
-  phpinfo();
-
-?>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="index.css">
+      <title>cs313 Kalbert Mata</title>
+  </head>
+  <body>
+    <header> 
+    <h3>CS 313 BYUI</h3>
+      <nav>
+          <ul class = "mainmenu">
+              <li><a href="index.php">Home</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="assingments.php">Assingments</a></li>
+          </ul>
+      </nav>
+    </header>
+    <main>
+      <h1>Kablbet Mata</h1>
+      <p>On this space i will talk about myself</p>
+      <img src="images/image1.jpg" alt="profile">
+    </main>
+    <script src="hamburger.js"></script>
+  
+  </body>
+  </html>
