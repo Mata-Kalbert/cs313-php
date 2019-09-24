243ae13938211266b9e096a9ca0b7a8709ec48f2
kalbert83 <mat17031@byui.edu>
2019-09-24 17:12:31 -0600

    testing

--- web/index.php
+++ web/index.php
@@ -32,8 +32,9 @@
     </header>
     <main>
       <h1>Kalbert Mata</h1>
-      <p>On this space i will talk about myself</p>
+    <p>My name is Kalbert Mata, I can say that baseball is my hobby. I love taking my two boys to their baseball practice and games, they are 8 and 7 years old. Last spring, we went to spring training in Arizona that was a lot of fun. </P>
       <img src="images/image1.jpg" alt="profile">
+    <p> I’m majoring on web development. I enjoyed learning new things, that’s why I have chosen this field of study. I know that going to be a challenge to keep up with new updates and new technologies that constantly flooding the market. My main goal on this semester besides learning as much as possible is to prepare a good portfolio that I can use on my job search.</P>
     </main>
     
   
