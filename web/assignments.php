09ccae58e5c1fa7158656a092c445d336693aaa4
kalbert83 <mat17031@byui.edu>
2019-09-26 00:32:04 -0600

    testing

--- web/assignments.php
+++ web/assignments.php
@@ -37,6 +37,14 @@
   
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
+  <footer>
+  <?php
+      echo '<div class="footer">';
+      echo "<p>Today is " . date("m.d.Y") . "</p>";
+      date_default_timezone_set('MST');
+      echo "<p>The time is " . date("h:i:sa") . "</p>";
+      echo "</div>";
+      ?>
+  </footer>
   </body>
   </html>
