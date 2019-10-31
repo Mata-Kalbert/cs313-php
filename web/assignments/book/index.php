93cce2df31a821c4b39f6013a2d96be53237ad0b
kalbert83 <mat17031@byui.edu>
2019-10-31 17:12:20 -0600

    update

--- web/assignments/book/index.php
+++ web/assignments/book/index.php
@@ -8,35 +8,26 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link type="text/css" rel="stylesheet" href="main.css">
-
         <title>Book Manager</title>
         </head>
     <body>
         <header>
-            <!--<nav class="navbar navbar-inverse navbar-static-top" role="navigation"> -->
-			  <div class="menu">
-              <button id="click" class="dropbtn">Book Reader</button>
-               <div id="dropdown" class="dropdown-content">
-               <!--<div class="navbar-header"> -->
-                    <a href="index.php">Home</a>
-                    <a href="checkout.php">Cheked Out</a>
-                    <a href="returned.php">Returned</a>
-                    <a href="addbook.php">Add New</a>
-                    <a href="return.php">Return</a>
-                </div> 
-                </div>         
-               <!-- </ul>
-            </nav> -->
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="checkout.php">Cheked Out</a></li>
+                    <li><a href="returned.php">Returned</a></li>
+                    <li><a href="addbook.php">Add New</a></li>
+                    <li><a href="return.php">Return</a></li>
+                </ul>
+            </nav>
         </header>
         <main>
-            
+            <h1>Book Manager</h1>
+            <p>Welcome to the Book Manager! Here you can keep track of the books you are currently readin, and of the ones you have already read.</p>
         </main>
-        <script src="books_menu.js"></script>
         <footer>
-    
-            <p>Welcome to the Book Reader! Here you can keep track of the books you are currently readin, and of the ones you have already read.</p>
             <p>CS 313 Kalbert Mata 2019</p>
         </footer>
-        
     </body>
 </html>
