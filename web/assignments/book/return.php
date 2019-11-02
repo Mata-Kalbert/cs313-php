ad41a612f095379d38961d4dde4ed199e7d624a1
kalbert83 <mat17031@byui.edu>
2019-11-01 22:40:27 -0600

    update

--- web/assignments/book/return.php
+++ web/assignments/book/return.php
@@ -13,7 +13,6 @@ session_start();
     </head>
     <body>
         <header>
-        <nav>
            <div>
              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
@@ -25,7 +24,6 @@ session_start();
                 </ul>
             </div>
         </div>
-        </nav>
         </header>
         <main>
             <h1>Book Manager</h1>
