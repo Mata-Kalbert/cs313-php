8e493ff358cd733435465265f9bbfc39a70c7c28
kalbert83 <mat17031@byui.edu>
2019-11-02 09:49:40 -0600

    update

--- web/assignments/book/checkout.php
+++ web/assignments/book/checkout.php
@@ -68,6 +68,29 @@ session_start();
             }
             
             ?>
+
+           <form action="delete_book_read.php" method="post">
+                <label>Title</label>
+                <?php
+                 ini_set('display_errors', 1);
+                require('dbConnect.php');
+                $db = get_db();
+
+                $query = $db->query("SELECT booksid, title FROM Books WHERE finnish = 'false';"); // Run your query
+
+                echo '<select name="titlebook">'; // Open your drop down box
+
+                // Loop through the query results, outputing the options one by one
+                while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                    
+                echo "<option value='" .$row['booksid'] . "'>".$row['title'].'</option>';
+                }
+
+                echo '</select>';
+
+                ?>
+                <input type="submit" value="Return book">
+            </form>
         </main>
         <footer>
             <p>CS 313 Kalbert Mata 2019</p>
